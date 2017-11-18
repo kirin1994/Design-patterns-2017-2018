7af0ebb305d809d4f37361bf85b0d18fe26de5a2
Adrian Podlawski <apodlawski@gmail.com>
2017-11-18 23:22:21 +0100

    Clean main

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -11,29 +11,7 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            var db = dbConnectSingletonLock.Instance;
-            var age = db.GetAge("Adrian");
 
-            Console.WriteLine($"age{age}");
-
-            var db2 = dbConnectSingletonDblCheck.Instance;
-            var age2 = db.GetAge("Adrian");
-
-            Console.WriteLine($"age{age2}");
-
-            var db3= dbConnectSingleton.Instance;
-            var age3 = db.GetAge("Adrian");
-
-            Console.WriteLine($"age{age3}");
-
-            var db4 = dbConnectSingletonNoLockSafe.Instance;
-            var age4 = db.GetAge("Adrian");
-
-            Console.WriteLine($"age{age4}");
-
-
-            Console.ReadKey();
-           
         }
     }
 }
